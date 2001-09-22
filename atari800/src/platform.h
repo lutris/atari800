d1c1f8689e9d90aaac83df0ba42fedb3c286f68b
fox <>
2001-09-22 09:22:37 +0000

    Atari_CONSOL -> key_consol

--- atari800/src/platform.h
+++ atari800/src/platform.h
@@ -16,7 +16,6 @@ void Atari_DisplayScreen (UBYTE *screen);
 int Atari_PORT(int num);
 int Atari_TRIG(int num);
 int Atari_POT(int num);
-int Atari_CONSOL(void);
 int Atari_PEN(int vertical);
 #if defined(SET_LED) && defined(NO_LED_ON_SCREEN)
 void Atari_Set_LED(int how);
@@ -26,4 +25,7 @@ void Atari_Set_LED(int how);
 
 /*
 $Log$
+Revision 1.2  2001/03/18 06:34:58  knik
+WIN32 conditionals removed
+
 */
