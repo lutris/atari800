11f97242bd6b0e1c5ac23ab51dd924f3353b463b
pfusik <>
2005-10-19 21:21:19 +0000

    removed #include "rt-config.h"

--- atari800/src/win32/sound.c
+++ atari800/src/win32/sound.c
@@ -38,7 +38,6 @@
 #include "pokeysnd.h"
 #include "atari.h"
 #include "log.h"
-#include "rt-config.h"	/* 'enable_new_pokey' defined there */
 
 #define MIXBUFSIZE	0x1000
 #define WAVSHIFT	9
@@ -511,6 +510,9 @@ void Sound_Continue(void)
 
 /*
 $Log$
+Revision 1.17  2005/09/07 22:03:40  pfusik
+fixed MSVC warnings
+
 Revision 1.16  2005/05/13 23:28:15  emuslor
 -help cleanup
 
